bad1d2d6670956353d6bfa149786022356c16475
AmirHShabani <97363432+AmirHShabani@users.noreply.github.com>
2024-02-26 23:38:05 +0330

    "Completed Assignment  1"

--- src/main/java/Exercises1.java
+++ src/main/java/Exercises1.java
@@ -3,8 +3,13 @@ public class Exercises1 {
     implement a function that returns factorial of given n
      */
     public long factorial(int n) {
-        // TODO
-        return 0;
+        int f = 1; 
+
+        for (int i = n; i > 1; i--) { 
+            f *= i;
+        }
+
+        return f;
     }
 
 
@@ -13,8 +18,20 @@ public class Exercises1 {
     the series -> 1, 1, 2, 3, 5, 8, ...
     */
     public long fibonacci(int n) {
-        // TODO
-        return 0;
+        
+        int a = 0, b = 1, c; 
+        
+        if (n==0) {
+            return a; 
+        }
+
+        for (int i = n; i >= 2; i--){
+            c = a + b; 
+            a = b;
+            b = c;
+            
+        }
+        return b;
     }
 
 
@@ -30,13 +47,24 @@ public class Exercises1 {
 
      */
     public char[][] generateTriangle(int rows) {
-        // TODO
+        
+        for (int i = 0; i <= rows; i++) {
+            for (int j = i; j > 0; j--) {
+                System.out.print("*");
+
+            }
+            System.out.println(); 
+
+        }
         return null;
     }
 
 
     public static void main(String[] args) {
         // test your code here!
+        Exercises1 exercises = new Exercises1();
+        System.out.println(exercises.fibonacci(7)); 
+        // Expected output: 8
     }
 
 }
